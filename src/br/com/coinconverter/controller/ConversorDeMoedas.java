2b3dcf387f49618cbeffc86a7ff7cbba444d949e
adrielCarmo <adrieldovale7@gmail.com>
2024-06-23 02:31:55 -0400

    adicionando ao conversor de moedas possiveis excecoes a serem lancadas

--- src/br/com/coinconverter/controller/ConversorDeMoedas.java
+++ src/br/com/coinconverter/controller/ConversorDeMoedas.java
@@ -1,2 +1,17 @@
-package br.com.coinconverter.controller;public class ConversorDeMoedas {
+package br.com.coinconverter.controller;
+
+import br.com.coinconverter.model.MoedasExchangeAPI;
+
+import java.io.IOException;
+
+public class ConversorDeMoedas {
+    public Double taxaConversao;
+
+    public Double converteValorMoeda(String siglaMoedaBase, String siglaMoedaAlvo, double valor) throws IOException, InterruptedException {
+        MoedasExchangeAPI taxasConversaoMoeda = new BuscaValoresMoedas().buscaTaxasConversaoExchangeAPI(siglaMoedaBase);
+
+        this.taxaConversao = Double.parseDouble(taxasConversaoMoeda.conversion_rates().get(siglaMoedaAlvo));
+
+        return (this.taxaConversao * valor);
+    }
 }
