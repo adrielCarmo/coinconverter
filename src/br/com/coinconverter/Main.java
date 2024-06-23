9c67eff37e917c654ab51d18f8b1bc125eab3b77
adrielCarmo <adrieldovale7@gmail.com>
2024-06-23 02:34:36 -0400

    add classe principal

---
+++ src/br/com/coinconverter/Main.java
@@ -0,0 +1,11 @@
+package br.com.coinconverter;
+
+import br.com.coinconverter.controller.MenuPrincipal;
+
+public class Main {
+    public static void main(String[] args) {
+        MenuPrincipal menuPrincipal = new MenuPrincipal();
+
+        menuPrincipal.iniciar();
+    }
+}
